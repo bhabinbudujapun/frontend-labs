d3ff10a63f76d753010c4abd02558c9bf2d4c114
bhabinpun <poon.bhabin6332@gmail.com>
2025-07-10 22:28:32 +0545

    chore: only variables prefixed with VITE_ are exposed

--- auth-system/src/main.tsx
+++ auth-system/src/main.tsx
@@ -3,9 +3,17 @@ import router from "./router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENTID}
+    authorizationParams={{
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+    }}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Auth0Provider>
 );
